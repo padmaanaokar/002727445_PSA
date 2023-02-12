f7662408c530d778615757f45382bfa29e17876d
padmaanaokar <anaokar.p@northeastern.edu>
2023-02-11 22:37:55 -0500

    Assignment 4

--- src/main/java/edu/neu/coe/info6205/union_find/HWQUPC_Solution.java
+++ src/main/java/edu/neu/coe/info6205/union_find/HWQUPC_Solution.java
@@ -1,4 +1,43 @@
 package edu.neu.coe.info6205.union_find;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class HWQUPC_Solution {
+   static int union=0;
+    public static void main(String[] arg) {
+        findConnections(1000, 250, 1500);
+    }
+    public static void findConnections(int runs, int low, int high) {
+        for (int i = low; i <= high; i = i+low) {
+            int totalConn = 0;
+            for (int j = 0; j < runs; j++) {
+                totalConn+=count(i);
+            }
+            System.out.println("Connection generated for (N) " + i + " sites: " + totalConn/runs);
+            System.out.println("Number of Unions required for "+i+" sites are :"+union/runs);
+            union=0;
+        }
+    }
+
+    public static int count(int n) {
+        int pairs = 0;
+        UF_HWQUPC unionFind = new UF_HWQUPC(n);
+        Random r = new Random();
+        while(unionFind.components() > 1) {
+            int p = r.nextInt(n);
+            int q = r.nextInt(n);
+            pairs++;
+            if (!unionFind.isConnected(p, q)) {
+                unionFind.connect(p, q);
+                union++;
+            }
+        }
+        return pairs;
+    }
 }
+
+
+
+
+
--- src/main/java/edu/neu/coe/info6205/union_find/UF_HWQUPC.java
+++ src/main/java/edu/neu/coe/info6205/union_find/UF_HWQUPC.java
@@ -82,8 +82,13 @@ public class UF_HWQUPC implements UF {
         validate(p);
         int root = p;
         // FIXME
-        // END 
-        return root;
+        while (p != parent[p])
+            p = parent[p];
+        if (this.pathCompression) {
+            doPathCompression(root);
+        }
+        return p;
+        // END
     }
 
     /**
@@ -170,14 +175,34 @@ public class UF_HWQUPC implements UF {
 
     private void mergeComponents(int i, int j) {
         // FIXME make shorter root point to taller one
-        // END 
+        // END
+
+      int rootX = find(i);
+        int rootY = find(j);
+
+        if(height[rootX] < height[rootY]){
+            parent[rootX] = rootY;
+            height[rootY]+=height[rootX];
+        }else if(height[rootX] > height[rootY]){
+            parent[rootY] = rootX;
+            height[rootX]+=height[rootY];
+        }else{
+            parent[rootY] = rootX;
+            height[rootX]++;
+        }
+
     }
 
     /**
      * This implements the single-pass path-halving mechanism of path compression
      */
-    private void doPathCompression(int i) {
-        // FIXME update parent to value of grandparent
-        // END 
-    }
+    private void doPathCompression(int i){
+            // FIXME update parent to value of grandparent
+            // END
+            while (i != parent[i]) {
+                parent[i] = parent[parent[i]];
+                i = parent[i];
+
+            }
+        }
 }
